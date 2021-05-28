ade89eead7281793a60d304c62c49246f3aec956
lucasdiazcano <lucasdiazcano@hotmail.com>
2021-05-27 21:19:25 -0300

    user icon

--- techo-admin/components/Navbar.jsx
+++ techo-admin/components/Navbar.jsx
@@ -1,16 +1,16 @@
 import React from 'react'
 import LogoTecho from './LogoTecho'
-
+import { UserIcon } from '@heroicons/react/outline'
 
 const Navbar = () => {
     return (
         <header className='flex justify-between' >
-            <LogoTecho/>
-            <div className='pr-5'>
+            <LogoTecho />
+            <div className='pr-5 flex items-center'>
+                <UserIcon/>
                 <h2>Nombre del usuario</h2>
                 <button className='ml-3'>Log Out</button>
             </div>
-
         </header>
     )
 }
