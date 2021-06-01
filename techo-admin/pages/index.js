922f24b51e0baaf54fea97fc9c0ac84bc1dea44d
MarCortina <marmica24@hotmail.com>
2021-05-31 21:34:22 -0300

    login styles

--- techo-admin/pages/index.js
+++ techo-admin/pages/index.js
@@ -1,15 +1,19 @@
 import Head from 'next/head'
 import Navbar from '../components/Navbar'
+import Login from '../components/Login'
 
 export default function Home() {
   return (
     <div >
       <Head>
-        <title>TECHO - Admin</title>
         <link rel="icon" href="/favicon.ico" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" />
+        <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@600&display=swap" rel="stylesheet" />
+        <title>TECHO - Admin</title>
       </Head>
 
-      <Navbar/>
+      <Navbar />
+      <Login />
 
     </div>
   )
