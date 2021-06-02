ed325d7841bf0ae91cb3b1ad24e1d6a7079aef92
lucasdiazcano <lucasdiazcano@hotmail.com>
2021-06-02 18:41:08 -0300

    botones estilos

--- techo-admin/components/SidebarButtons.js
+++ techo-admin/components/SidebarButtons.js
@@ -3,7 +3,7 @@ import React from 'react'
 const SidebarButtons = () => {
     return (
         <div>
-            <button className='m-5'><h2> Botón sidebar </h2></button>
+            <button className='hover:bg-red-700 text-white font-bold py-2 px-4 p-5'><h2> Botón sidebar </h2></button>
             
         </div>
     )
