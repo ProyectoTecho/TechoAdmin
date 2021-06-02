556f39c347efd48f257b2292f521df731e205909
lucasdiazcano <lucasdiazcano@hotmail.com>
2021-06-02 10:42:28 -0300

    dejando lo de mar como estaba

--- techo-admin/components/Login.js
+++ techo-admin/components/Login.js
@@ -1,49 +1,37 @@
-import { useState } from 'react'
-import LogoTecho from './LogoTecho'
+import Image from "next/image";
 
 
 function Login() {
-   /*  const [data, setData] = useState({
-        name: "",
-        pass: "",
-    });
+  return (
+    <div className="bg-blue-500 h-screen sm:text-center text-center ">
 
-    const handleData = (e) => {
-        setData({ ...data, [e.target.name]: e.target.value });
-    };
-    
-    console.log(data) */
+      <form className="max-w-2xl mx-auto space-y-6 focus-border-teal-500  align-middle">
+        <h1 className="text-white text-4xl font-extrabold">ADMIN</h1>
+        <p>Ingreso solo para administradores</p>
 
-    return (
-        <div className="h-screen sm:text-center text-center ">
-
-            <form className="max-w-2xl mx-auto space-y-6 focus-border-teal-500  align-middle">
-                <h1 className="text-white text-4xl font-extrabold mt-5">ADMIN</h1>
-                <p>Ingreso solo para administradores</p>
-
-                <div>
-                    <input /* name='name' onChange={handleData} */ type="email" placeholder="EMAIL" className="form-input px-4 py-3 rounded-full 
+        <div>
+        <input type="email" placeholder="EMAIL" className="form-input px-4 py-3 rounded-full 
         border-none"></input>
-                </div>
-
-                <div>
-                    <input /* name='pass'onChange={handleData} */ type="password" placeholder="PASSWORD" className="form-input px-4 py-3 rounded-full 
-        border-none" ></input>
-                </div>
-
-                {/* flecha rebotando */}
-                <svg class="animate-bounce w-6 h-6"></svg>
-
-                <div>
-                    <button type="submit" className="rounded-full border-solid border-2 border-light-blue-500 text-1xl px-3 py-1 mb-5">INGRESAR</button>
-                </div>
-
-            </form>
-            <footer>
-                <LogoTecho />
-            </footer>
         </div>
-    );
+
+        <div>
+        <input type="password" placeholder="PASSWORD" className="form-input px-4 py-3 rounded-full 
+        border-none" ></input>
+        </div>
+
+        {/* flecha rebotando */}
+        <svg class="animate-bounce w-6 h-6"></svg> 
+        
+        <div>
+        <button type="submit" className="rounded-full border-solid border-2 border-light-blue-500 text-1xl px-3 py-1 ">INGRESAR</button>
+        </div>
+
+      </form>
+      <footer>
+        <Image src="/techo-footer.png" width="auto" height="100" />
+      </footer>
+    </div>
+  );
 }
 
 export default Login;
