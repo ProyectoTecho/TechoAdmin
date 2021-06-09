e56aceaa80165dcd0def7b9c47c6abed9948080b
lucasdiazcano <lucasdiazcano@hotmail.com>
2021-06-09 12:20:14 -0300

    eliminado input de sobra

--- techo-admin/components/Montos.js
+++ techo-admin/components/Montos.js
@@ -3,7 +3,11 @@ import Input from './Input'
 import Button from './Button'
 
 const Montos = () => {
-    const [value, setValue] = useState(0)
+    const [data, setValue] = useState({
+        firstInput: 0,
+        secondInput: 0,
+        thirdInput: 0,
+    })
 
     const onChange = (e) => {
         e.preventDefault()
@@ -12,15 +16,13 @@ const Montos = () => {
 
     return (
         
-        <div className='align-center w-36 m-5'>
+        <div className='align-center w-36 m-5 space-y-6'>
             <h1 className='font-bold'>Editar Montos</h1>
-            <Input type={'number'} value={value} onChange={onChange} placeholder={'Ingrese nuevo monto'} label={'1er Monto'}/>
-            <Input type={'number'} value={value} onChange={onChange} placeholder={'Ingrese nuevo monto'}
+            <Input type={'number'} value={data.firstInput} onChange={onChange} placeholder={'Ingrese nuevo monto'} label={'1er Monto'}/>
+            <Input type={'number'} value={data.secondInput} onChange={onChange} placeholder={'Ingrese nuevo monto'}
             label={'2do Monto'}/>
-            <Input type={'number'} value={value} onChange={onChange} placeholder={'Ingrese nuevo monto'}
+            <Input type={'number'} value={data.thirdInput} onChange={onChange} placeholder={'Ingrese nuevo monto'}
             label={'3er Monto'}/>
-            <Input type={'number'} value={value} onChange={onChange} placeholder={'Ingrese nuevo monto'}
-            label={'4to Monto'}/>
             <Button content='Guardar cambios' styles={"ml-3 rounded whitespace-nowrap group cursor-pointer transition duration-200 ease-in transform sm:hover:scale-105 hover:z-50 p-2.5"}/>
         </div>
     )
--- techo-admin/components/Sidebar.js
+++ techo-admin/components/Sidebar.js
@@ -6,7 +6,7 @@ import Montos from './Montos'
 const Sidebar = () => {
     return (
         <div className='flex '>           
-            <div className='bg-[#449DEF] space-y-8 mr-5 ml-5 w-36 mt-5  '>
+            <div className='bg-[#449DEF] space-y-8 mr-5 ml-5 w-36 mt-5 '>
                 <SidebarButtons title='Header' />
                 <SidebarButtons title='Numeros' />
                 <SidebarButtons title='Captadoras' />
