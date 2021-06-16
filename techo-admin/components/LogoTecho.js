3916cb608e3cddbae24d6e8ab57888c0f053aa08
lucasdiazcano <lucasdiazcano@hotmail.com>
2021-06-16 14:36:36 -0300

    href en el logo de la navbar

--- techo-admin/components/LogoTecho.js
+++ techo-admin/components/LogoTecho.js
@@ -1,11 +1,12 @@
 import React from "react";
 import Image from "next/image";
+import Link from 'next/link'
 
 const LogoTecho = ({ styles }) => {
   return (
     <div>
       <div className={styles}>
-        <Image src="/techo-footer.png" width="auto" height="100" />
+        <Link href="/Home"><Image src="/techo-footer.png" width="auto" height="100" /></Link>
       </div>
     </div>
   );
